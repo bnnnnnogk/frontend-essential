ad471e5ce5f7d44119286c8102a00449111cc949
Konz <konz@Konzs-MacBook-Air.local>
2023-09-21 16:16:40 +0700

    upate code with localStorage

--- hands-on-5/app.js
+++ hands-on-5/app.js
@@ -20,11 +20,10 @@ listContainer.addEventListener(
   function (e) {
     if (e.target.tagName === "LI") {
       e.target.classList.toggle("checked");
-      saveData();
     } else if (e.target.tagName === "SPAN") {
       e.target.parentElement.remove();
-      saveData();
     }
+    saveData();
   },
   false
 );
